628dfaf7a82258a9652de90d3caa7037ea5a7462
abio123 <balikisabioye35@gmail.com>
2022-10-27 05:54:25 -0700

    Strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * @src: copy from
  * Return: pointer to dest
  */
-char *_strcat(char *dest, char *src)
+char _strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -25,3 +25,4 @@ char *_strcat(char *dest, char *src)
 	}
 	dest[i] = '\0';
 	return (dest);
+}
