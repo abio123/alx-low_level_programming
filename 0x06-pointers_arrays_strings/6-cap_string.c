4e77524e9531c2095a42f6b1bd5c9a1bc5079592
abio123 <balikisabioye35@gmail.com>
2022-10-27 15:08:24 -0700

    Cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++
+			i++;
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
 		    str[i - 1] == '\n' ||
@@ -34,3 +34,4 @@ char *cap_string(char *str)
 
 	return (str);
 }
+
